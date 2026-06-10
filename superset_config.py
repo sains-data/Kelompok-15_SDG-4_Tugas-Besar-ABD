242273777b30515639221ce55fb82f0f0c0f7b4d
juesiaprideliasaragih <juesisaragih99@gmail.com>
2026-06-10 21:46:04 +0700

    Add files via upload

---
+++ superset_config.py
@@ -0,0 +1,19 @@
+EXTRA_CATEGORICAL_COLOR_SCHEMES = [
+    {
+        "id": "colorhunt_ocean",
+        "description": "ColorHunt Ocean Palette",
+        "label": "ColorHunt Ocean",
+        "isDefault": False,
+        "colors": ["#355872", "#426e8e", "#5084aa", "#609bc6", "#7aaace", "#8bc1e6", "#9cd5ff", "#bae1ff", "#dcf0ff", "#f7f8f0"]
+    }
+]
+
+EXTRA_SEQUENTIAL_COLOR_SCHEMES = [
+    {
+        "id": "colorhunt_ocean_seq",
+        "description": "ColorHunt Ocean Sequential",
+        "label": "ColorHunt Ocean Seq",
+        "isDefault": False,
+        "colors": ["#f7f8f0", "#dcf0ff", "#bae1ff", "#9cd5ff", "#8bc1e6", "#7aaace", "#609bc6", "#5084aa", "#426e8e", "#355872"]
+    }
+]
